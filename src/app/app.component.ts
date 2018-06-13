e995a76df2f76778b81cd4b9b03eb4be99061d73
Bren Gunning <github@cinnamonrage.com>
2018-06-13 10:52:03 +0100

    Fixing issues with how it appears on Safari.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,59 @@
 import { Component } from '@angular/core';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-root',
   template: `
     <main-nav>
     </main-nav>
+    
+    <a download="SafariDownloadProblem1.pdf" (click)="downloadFilePost()">Download current way (POST)</a>
+    <a download="SafariDownloadProblem2.pdf" (click)="downloadFileGet()">Download current way (GET)</a>
+    <a download="SafariDownloadProblem3.pdf" href="http://safaridownloadissue-api.azurewebsites.net/api/download/" >
+    Download via href (GET)</a>
   `,
   styles: []
 })
 export class AppComponent {
   title = 'app';
+
+  downloadFilePost() {
+    console.log('Testing download');
+    const downloadUrl = `http://safaridownloadissue-api.azurewebsites.net/api/download/`;
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', downloadUrl, true);
+    xhr.setRequestHeader('Random-Header-String', 'Yolo');
+    xhr.responseType = 'blob';
+    xhr.withCredentials = true;
+    xhr.onprogress = (event) => {
+      console.log(event.loaded / event.total * 100);
+    };
+    xhr.onload = function (e: Event) {
+      if (xhr.status === 200) {
+        FileSaver.saveAs(xhr.response,  `SafariDownloadProblem.pdf`);
+      } else {
+      }
+    };
+    xhr.send();
+  }
+
+  downloadFileGet() {
+    console.log('Testing download');
+    const downloadUrl = `http://safaridownloadissue-api.azurewebsites.net/api/download/`;
+    const xhr = new XMLHttpRequest();
+    xhr.open('Get', downloadUrl, true);
+    xhr.setRequestHeader('Random-Header-String', 'Yolo');
+    xhr.responseType = 'blob';
+    xhr.withCredentials = true;
+    xhr.onprogress = (event) => {
+      console.log(event.loaded / event.total * 100);
+    };
+    xhr.onload = function (e: Event) {
+      if (xhr.status === 200) {
+        FileSaver.saveAs(xhr.response,  `SafariDownloadProblem.pdf`);
+      } else {
+      }
+    };
+    xhr.send();
+  }
 }
